288bf8dad04741e31570ca7a947f7ee4f3e10e4e
GopalGavas <gopalgavas25@outlook.com>
2024-10-01 21:26:51 +0530

    Add utils and middlewares

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 import express from "express";
 import logger from "./utils/logger.js";
 import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+// "<------- MIDDLEWARES ------->"
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
+app.use(express.json({}));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+// "<------- LOGGER WITH MORGAN AND WINSTON ------->"
 const morganFormat = ":method :url :status :response-time ms";
 
 app.use(
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,52 @@
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+import { ApiError } from "./apiError";
+
+cloudinary.config({
+  cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`,
+  api_key: `${process.env.CLOUDINARY_API_KEY}`,
+  api_secret: `${process.env.CLOUDINARY_API_SECRET}`, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log(
+      "File has been successfully uploaded on cloudinary",
+      response.url
+    );
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    console.log(`Something went wrong while uploading on Cloudinary`);
+    return null;
+  }
+};
+
+const deleteOnCloudinary = async (publicId, resource_type = "image") => {
+  try {
+    if (!publicId) return null;
+    const response = await cloudinary.uploader.destroy(publicId, {
+      resource_type: `${resource_type}`,
+    });
+
+    console.log("Old Image deleted successfully");
+    return response;
+  } catch (error) {
+    throw new ApiError(500, {
+      message:
+        "Something went wrong while deleting image from cloudinary" ||
+        error.message,
+    });
+  }
+};
+
+{
+  uploadOnCloudinary, deleteOnCloudinary;
+}
