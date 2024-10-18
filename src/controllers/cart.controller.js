6a191bcc88e4f1b93755408ec98697fef87269ef
GopalGavas <gopalgavas25@outlook.com>
2024-10-18 13:33:51 +0530

    add cart controllers and endpoints

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -60,6 +60,65 @@ const addToCart = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, cart, "Product added to cart successfully"));
 });
 
+const updateCart = asyncHandler(async (req, res) => {
+  const { prodId, count, color } = req.body;
+
+  if (!prodId) {
+    throw new ApiError(400, "Invalid Product Id");
+  }
+
+  const product = await Product.findById(prodId);
+
+  if (!product) {
+    throw new ApiError(404, "Product not found");
+  }
+
+  const cart = await Cart.findOne({ orderBy: req.user?._id });
+
+  if (!cart) {
+    throw new ApiError(404, "Cart not found");
+  }
+
+  const productIndex = cart.products.findIndex(
+    (p) => p.product.toString() === prodId
+  );
+
+  if (productIndex === -1) {
+    throw new ApiError(404, "Product is not in the cart");
+  }
+
+  if (color) {
+    cart.products[productIndex].color = color;
+  }
+  if (count) {
+    cart.products[productIndex].count = count;
+  }
+
+  cart.cartTotal = cart.products.reduce(
+    (acc, prod) => acc + prod.price * prod.count,
+    0
+  );
+
+  if (cart.appliedCoupons && cart.appliedCoupons.length > 0) {
+    const coupon = await Coupon.findById(cart.appliedCoupons[0]);
+    let currentDate = Date.now();
+    if (coupon.expiry && coupon.expiry < currentDate) {
+      throw new ApiError(400, "Coupon is expired");
+    }
+    const discount = coupon.discount / 100;
+    const priceAfterDiscount = cart.cartTotal - cart.cartTotal * discount;
+    cart.TotalAfterDiscount = priceAfterDiscount;
+  } else {
+    cart.TotalAfterDiscount = null;
+  }
+
+  await cart.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, cart, "cart updated successfully"));
+});
+
 const removeFromCart = asyncHandler(async (req, res) => {
   const { prodId } = req.body;
 
@@ -142,4 +201,112 @@ const applyCouponToCart = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, cart, "Coupon applied successfully"));
 });
 
-export { addToCart, removeFromCart, applyCouponToCart };
+const removeCouponFromCart = asyncHandler(async (req, res) => {
+  const cart = await Cart.findOne({ orderBy: req.user?._id });
+
+  if (!cart) {
+    throw new ApiError(404, "Cart not found");
+  }
+
+  cart.TotalAfterDiscount = null;
+  cart.appliedCoupons = [];
+
+  await cart.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, cart, "Coupon removed from the cart"));
+});
+
+const getCart = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+
+  const cart = await Cart.aggregate([
+    {
+      $match: {
+        orderBy: userId,
+      },
+    },
+    {
+      $unwind: "$products",
+    },
+    {
+      $lookup: {
+        from: "products",
+        localField: "products.product",
+        foreignField: "_id",
+        as: "productDetails",
+      },
+    },
+    {
+      $addFields: {
+        productDetail: {
+          $first: "$productDetails",
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$_id",
+        products: {
+          $push: {
+            product: "$productDetail",
+            count: "$products.count",
+            color: "$products.color",
+            price: "$products.price",
+          },
+        },
+        cartTotal: { $first: "$cartTotal" },
+        appliedCoupons: { $first: "$appliedCoupons" },
+        TotalAfterDiscount: { $first: "$TotalAfterDiscount" },
+      },
+    },
+    {
+      $project: {
+        "products.product.title": 1,
+        "products.product.brand": 1,
+        "products.count": 1,
+        "products.color": 1,
+        "products.price": 1,
+        cartTotal: 1,
+        appliedCoupons: 1,
+        TotalAfterDiscount: 1,
+      },
+    },
+  ]);
+
+  if (!cart || cart.length === 0) {
+    throw new ApiError(404, "Cart not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, cart, "Cart fetched successfully"));
+});
+
+const clearCart = asyncHandler(async (req, res) => {
+  const cart = await Cart.findOne({ orderBy: req.user?._id });
+
+  if (!cart) {
+    throw new ApiError(404, "Cart not found");
+  }
+
+  cart.products = [];
+  cart.cartTotal = 0;
+  cart.appliedCoupons = [];
+  cart.TotalAfterDiscount = null;
+
+  await cart.save();
+
+  return res.status(200).json(new ApiResponse(200, cart, "Cart cleared"));
+});
+
+export {
+  addToCart,
+  updateCart,
+  removeFromCart,
+  applyCouponToCart,
+  removeCouponFromCart,
+  getCart,
+  clearCart,
+};
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 import {
   addToCart,
   applyCouponToCart,
+  clearCart,
+  getCart,
+  removeCouponFromCart,
   removeFromCart,
+  updateCart,
 } from "../controllers/cart.controller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 
@@ -11,7 +15,11 @@ const router = Router();
 router.use(verifyJwt);
 
 router.route("/").post(addToCart);
+router.route("/").get(getCart);
+router.route("/update").patch(updateCart);
 router.route("/remove-product").delete(removeFromCart);
 router.route("/coupon").post(applyCouponToCart);
+router.route("/remove-coupon").patch(removeCouponFromCart);
+router.route("/clear").delete(clearCart);
 
 export default router;
